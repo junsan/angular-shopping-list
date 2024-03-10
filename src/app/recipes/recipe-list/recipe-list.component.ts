61e2a8effa1b98a5f4b7b666a99d65b4136be379
Jun Santos <xjunsantos@gmail.com>
2024-03-10 17:36:25 +0800

    ingredient model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe('Chicken Inasal', 'This is a description', 'https://bing.com/th?id=OSK.abe1187979a3b700cb737c3b452196cf'),
+    new Recipe('Chicken Inasal', 'This is a description', 'https://bing.com/th?id=OSK.abe1187979a3b700cb737c3b452196cf')
+  ]
 }
